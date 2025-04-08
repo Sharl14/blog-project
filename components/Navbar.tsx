d6e179573209496f4aa19a2fb579e3c77d8a21eb
Sharl14 <sharlenepillay14@gmail.com>
2025-04-08 14:34:23 +0200

    initial project setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
+const navbar = () => {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <h1 className="text-3xl font-semibold">
+          Next <span className="text-blue-600">Blog</span>
+        </h1>
+
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/dashboard"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4"></div>
+    </nav>
+  );
+};
+
+export default navbar;
